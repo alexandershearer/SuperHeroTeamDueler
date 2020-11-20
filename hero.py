e0914d26bf7589116a07cb56787cd2a969d47604
alexandershearer <alex.shearer23@gmail.com>
2020-11-20 02:47:11 -0600

    Finished armor and ability files. Working on damage taken

--- hero.py
+++ hero.py
@@ -1,22 +1,41 @@
 import random
+from ability import Ability
 
 class Hero:
     def __init__(self, name, starting_health=100):
+        self.abilities = list()
+        self.armors = list()
         self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
 
+    def add_ability(self, ability):
+        self.abilities.append(ability)
+
+    def attack(self):
+        total_damage = 0
+        for ability in self.abilities:
+            total_damage += ability.attack()
+        return total_damage
+
+    def add_armor(self, armor):
+        self.armors = armor
+
+    def defend(self, damage_amt):
+        total_armor = 0
+        for armor in self.armors:
+            total_armor += armor.defend()
+        return total_armor
+
     def fight(self, opponent):
-    # TODO: Fight each hero until a victor emerges.
-    # Phases to implement:
-    #1) randomly choose winner,
-    #Hint: Look into random library, more specifically the choice method
         print(f'{random.choice([self.name, opponent])} won!')
 
 if __name__ == "__main__":
     # If you run this file from the terminal
-    # this block is executed.
-    hero1 = Hero("Wonder Woman")
-    hero2 = Hero("Dumbledore")
-
-    hero1.fight(hero2)
+    # this block of code is executed.
+    ability = Ability("Great Debugging", 50)
+    another_ability = Ability("Smarty Pants", 90)
+    hero = Hero("Grace Hopper", 200)
+    hero.add_ability(ability)
+    hero.add_ability(another_ability)
+    print(hero.attack())
